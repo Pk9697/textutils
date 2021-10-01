f74ba83c02ee7657ff5fb3f328448b2e02579a01
Prashant Kumar <prashant.aman9@gmail.com>
2021-10-01 13:50:48 +0530

    add dark mode functionality from Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
+import { useState } from "react/cjs/react.development";
 import Navbar from "./Navbar";
 import TextForm from "./TextForm";
+import '../App.css'
 function App() {
+  const [mode,setMode]=useState('light');
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#000000';
+      document.body.style.color='white';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      document.body.style.color='black';
+    }
+
+  }
+
   return (
     <>
-      <Navbar title="TextUtils2"/>
-      <div className="container my-3">
-          <TextForm header="Enter your text here"/>
+      <Navbar id="navbar" title="TextUtils2" mode={mode} toggleMode={toggleMode}/>
+      <div className="container py-3" id="textform" >
+          <TextForm header="Enter your text here"  mode={mode}/>
       </div>
-      
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -35,56 +35,56 @@ export default function TextForm(props) {
         document.querySelector('#copy-btn').innerText = 'Copied';
     }
 
-    const toggleStyle=()=>{
-        console.log("clicked");
-        if(myStyle.color==='black'){
-            setMyStyle({
-                color:'white',
-                backgroundColor:'black'
-            })
-            setBtnText('Disable Dark Mode');
-        }else{
-            setMyStyle({
-                color:'black',
-                backgroundColor:'white'
-            })
-            setBtnText('Enable Dark Mode');
-        }
-    }
+    // const toggleStyle=()=>{
+    //     console.log("clicked");
+    //     if(myStyle.color==='black'){
+    //         setMyStyle({
+    //             color:'white',
+    //             backgroundColor:'black'
+    //         })
+    //         setBtnText('Disable Dark Mode');
+    //     }else{
+    //         setMyStyle({
+    //             color:'black',
+    //             backgroundColor:'white'
+    //         })
+    //         setBtnText('Enable Dark Mode');
+    //     }
+    // }
 
-    const [myStyle,setMyStyle]=useState({
-        color:'black',
-        backgroundColor:'white'
-    })
+    // const [myStyle,setMyStyle]=useState({
+    //     color:'black',
+    //     backgroundColor:'white'
+    // })
 
-    const [btn,setBtnText]=useState('Enable Dark Mode');
+    // const [btn,setBtnText]=useState('Enable Dark Mode');
 
 
     const [text,setText] = useState("Enter text here");
     return (
         <>
-        <div className="container form-check form-switch">
+        {/* <div className="container form-check form-switch">
             <input className="form-check-input" onClick={toggleStyle} type="checkbox" id="flexSwitchCheckDefault"/>
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{btn}</label>
-        </div>
+        </div> */}
 
-        <div className="container" style={myStyle}>
+        <div className="container"  >
             <h1>{props.header}</h1>
-            <div className="mb-3" style={myStyle}>
+            <div className="mb-3" >
                 <label htmlFor="myBox" className="form-label">Example textarea</label>
-                <textarea style={myStyle} className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea  style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary me-2 mb-2" onClick={handleUpClick}>Convert to Upper case</button>
             <button className="btn btn-primary me-2 mb-2" onClick={handleLoClick}>Convert to Lower case</button>
             <button className="btn btn-primary me-2 mb-2" onClick={handleClearClick}>Clear</button>
             <button className="btn btn-primary me-2 mb-2" id="copy-btn" onClick={handleCopy}>Copy Text</button>
         </div>
-        <div className="container my-3" style={myStyle}>
+        <div className="container my-3" >
             <h2>Your Text Summary</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008*text.split(" ").length} minutes to read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter something in the text box above to preview"}</p>
         </div>
         </>
     )
