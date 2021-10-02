77214400fe45d00380033574d667459ad2e659df
Prashant Kumar <prashant.aman9@gmail.com>
2021-10-02 13:23:16 +0530

    fix count no. of words without using regx

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -85,7 +85,7 @@ export default function TextForm(props) {
         </div>
         <div className="container my-3" >
             <h2>Your Text Summary</h2>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{text.split(" ").filter(function(n){return n!==''}).length} words and {text.length} characters</p>
             <p>{0.008*text.split(" ").length} minutes to read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Enter something in the text box above to preview"}</p>
