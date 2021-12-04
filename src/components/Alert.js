89bd07118709b90b2f9f1b62b4f027a1e8fb0c08
Prashant Kumar <prashant.aman9@gmail.com>
2021-12-04 20:46:31 +0530

    fix bugs in Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,7 +9,7 @@ function Alert(props) {
 
     return (
         <div style={{height:'50px'}}>
-            {props.alert && <div class={`alert alert-${props.alert.type}`} role="alert">
+            {props.alert && <div className={`alert alert-${props.alert.type}`} role="alert">
                 <strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg}
             </div>}
         </div>
